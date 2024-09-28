e071be43c238066fc5e612959ce40d01b25ac9b0
Muhammadsameergit <160648778+muhammadsameer1257@users.noreply.github.com>
2024-09-28 06:01:58 +0500

    Add files via upload

---
+++ assignment 4/script.js
@@ -0,0 +1,14 @@
+var ages = Number(prompt("Enter your Age"));
+
+if (ages >= 65) {
+    alert("You are Senior!");
+}
+else if (ages >= 20) {
+    alert("You are Adult!");
+}
+else if (ages >= 13) {
+    alert("You are Teenager!");
+}
+else if (ages >= 1) {
+    alert("You are Child!");
+}
